2fd85ac10ccdc91999d048df1ee7970b96c4556d
Pavlo Tymchuk <pawel.tymczuk@gmail.com>
2017-10-04 23:06:57 +0200

    - added configuration server - Spring Cloud Config; - configured discovery server - Eureka; - configured quiz service to have test endpoint /demo which returns value from added in configuration on config server;

--- quiz-service/src/main/java/com/grapeup/quizservice/QuizServiceApplication.java
+++ quiz-service/src/main/java/com/grapeup/quizservice/QuizServiceApplication.java
@@ -1,14 +1,26 @@
 package com.grapeup.quizservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient // it will register this service with eureka
+@RestController
 public class QuizServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizServiceApplication.class, args);
+	}
+
+	@Value("${some.value}")
+	private String value;
+
+	@GetMapping(path = "/demo")
+	public String getProp() {
+		return value;
 	}
 }
